eade31a3057a345c784adfa618bf53c78b37ad8d
MikeTheNerd <miketherealnerd@gmail.com>
2023-03-17 08:57:38 +0100

    Set the project structure

---
+++ TypeBinary/TYBI/TybiProject.cs
@@ -0,0 +1,16 @@
+using Tomlet;
+
+namespace TypeBinary.TYBI;
+
+public class TybiProject
+{
+    public TybiProject()
+    {
+        
+    }
+
+    public static TybiProject? OpenProject()
+    {
+
+    }
+}
